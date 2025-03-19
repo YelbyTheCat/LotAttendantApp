d993485d820232db4634f4a3c881b8cb9fde61d4
YelbyTheCat <bradersonman@gmail.com>
2025-03-18 20:54:21 -0400

    Prepwork

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { StyleSheet } from 'react-native';
 
-
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
